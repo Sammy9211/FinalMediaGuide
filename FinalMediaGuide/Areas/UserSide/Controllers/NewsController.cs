2b820cb155f8a7c0b7393b16bac611a84b7e65c5
Davit Karapetyan <davit207karapetyan@gmail.com>
2022-11-29 19:51:00 +0400

    changes

---
+++ FinalMediaGuide/Areas/UserSide/Controllers/NewsController.cs
@@ -0,0 +1,21 @@
+﻿using FinalMediaGuide.BLL.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalMediaGuide.Areas.UserSide.Controllers
+{
+    [Area("UserSide")]
+    public class NewsController : Controller
+    {
+        private readonly INewsService _newsService;
+        public NewsController(INewsService newsService)
+        {
+            _newsService = newsService;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _newsService.GetAllNews();
+            return View(data);
+        }
+    }
+}
